cefcadd4222127e55c51a88cffd980566fc2fa9b
Leylan Aurakeasamy <leylan.aurakeasamy1@gmail.com>
2020-06-22 20:36:04 +0200

    Upload remainder of files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React,{Component} from 'react';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+  
+    this.state={
+      current:'',
+      previous:[]
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <input className="result" value={this.state.current}/>
+      </div>
+    );
+    }
+
+}
+export default App;
