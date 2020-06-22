e0a41a57c9e892e693d75723673227ca68fe2f5a
Leylan Aurakeasamy <leylan.aurakeasamy1@gmail.com>
2020-06-22 21:16:52 +0200

    Added beginning of calculator buttons. Need to still update button class to get correct button

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Button extends Component{
+    render(){
+        return(
+        <div>
+            <button className="calcButton">"1"</button>
+        </div>
+        )
+    }
+}
+
+export default Button;
